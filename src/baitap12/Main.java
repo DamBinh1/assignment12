2b7565ecdcf2c49f4f8d865951c417dbb4cc65f2
DamBinh1 <118426540+DamBinh1@users.noreply.github.com>
2023-05-12 22:46:20 +0700

    Create Main.java

---
+++ src/baitap12/Main.java
@@ -0,0 +1,12 @@
+package baitap12;
+
+public class Main {
+    public static void main(String[] args) {
+        News n1 = new News(123,"Anh hung xa dieu","22/5/2022","Kim Dung","fhsoafhsio");
+        News n2 = new News(124,"Kung Fu Panda","23/5/2022","Nam Coi","grggsgffs");
+        n1.InputRate();
+        n1.Display();
+        n2.InputRate();
+        n2.Display();
+    }
+}
